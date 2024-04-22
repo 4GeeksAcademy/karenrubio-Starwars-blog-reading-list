e2a1ec7e38e667842c15b021c697fbf401884c9f
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-04-22 18:01:00 +0000

    Eliminacion de boton favoritos extra

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -21,14 +21,6 @@ export const Navbar = (props) => {
 			<img className="navbar-brand mb-0 h1" src={"https://pngimg.com/uploads/star_wars_logo/star_wars_logo_PNG18.png"} style={{width: "100px", height:"100px"}}/>
 			</Link>
 			<div className="p-3 mb-2 bg-dark-subtle text-dark-emphasis">
-				<button type="button" className="btn btn-primary">
-  					Favorites <span className="badge text-bg-secondary">{store.characthersLiked.length}</span>
-				</button>
-				<div className="border border-warning-subtle">
-					
-					{store.characthersLiked.map((elemento,index)=><div className="d-flex justify-content-between m-2 " key={index}><p >{elemento}</p><i onClick={()=>actions.deleteFavorite(elemento)} className="bi bi-trash3-fill" ></i></div>)}
-				</div>
-				
 					<div className="btn-group">
   						<button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
   							  Favorites <span className="badge text-bg-secondary">{store.characthersLiked.length}</span>
